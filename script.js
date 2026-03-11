48493f9fa2fc3a35246c784b20aa6aa2a2ee83bd
coddriwaj <pudasainixyz@gmail.com>
2026-03-11 19:37:25 +0545

    ..

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 element=document.querySelector(".element");
 element.onclick=(e)=>{
-   /* response=fetch("https://acousticbrainz.org/98567252-2622-4e0f-b7bd-6ab93ef2f678/low-level");
-    data=response.json();
-    console.log(data);*/
+   
     console.log(e);
 
 }
